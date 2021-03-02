25aadb7bde2c24ec233fd08ba6836cb11a7d24db
saraPiresDev <sararegina.pires@gmail.com>
2021-03-02 01:01:53 -0300

    Sara Pires - Criando as Classes e entidades.

---
+++ easyinsurance-planos-microservice/src/main/java/br/com/fiap/microservices/easyinsurance/planos/PlanosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.microservices.easyinsurance.planos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanosApplication.class, args);
+	}
+
+}
---
+++ easyinsurance-planos-microservice/src/test/java/br/com/fiap/microservices/PlanosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
